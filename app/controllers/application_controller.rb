44d24def47fbf913102520434d9abc9d6b243c5c
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-30 18:33:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,6 +44,8 @@ class ApplicationController < Sinatra::Base
      @recipe = Recipe.find(params['id'])
       @recipe.update(name: params['name'], ingredients: params['ingredients'], cook_time: params['cook_time'])
     redirect "/recipes/#{@recipe.id}"
-  end
+  end 
+  
+  delete ''
 
 end
